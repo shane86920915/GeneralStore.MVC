16c062b68b24736e50133c35d617e1a8a575ce06
shane86920915 <73564809+shane86920915@users.noreply.github.com>
2021-03-30 19:05:34 -0400

    Add project files.

---
+++ GeneralStore.MVC/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using GeneralStore.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStore.MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ApplicationDbContext _db = new ApplicationDbContext();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View(_db.products.ToList());//Returns the created products from database
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.products.Add(product);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        //get : Delete
+
+    }
+}
